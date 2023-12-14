b001f12a8af5280193904abb43512121f08b3d6b
rosanno <rosannomacabuhay@gmail.com>
2023-12-14 21:18:58 +0800

    update

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,9 +1,27 @@
+import axios from "axios";
+
+import { Brands } from "@/components/brand";
 import { Featured } from "@/components/featured";
 
-export default function Home() {
+async function getBrands() {
+  try {
+    const res = await axios.get(
+      "http://localhost:3000/api/brands"
+    );
+
+    return res.data;
+  } catch (error) {
+    throw new Error("Request failed.");
+  }
+}
+
+export default async function Home() {
+  const brands = await getBrands();
+
   return (
-    <div>
+    <div className="px-4 sm:px-10 mt-10 md:mt-28">
       <Featured />
+      <Brands brands={brands} />
     </div>
   );
 }

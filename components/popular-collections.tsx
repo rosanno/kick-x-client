7a875d6787e958d0958fe97d9be8d5674757b433
rosanno <rosannomacabuhay@gmail.com>
2023-12-15 13:48:23 +0800

    create popular-collections components

--- components/popular-collections.tsx
+++ components/popular-collections.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { ShoppingBag } from "lucide-react";
+import Image from "next/image";
+
+import { formatter } from "@/lib/utils";
 import { Products } from "@/types";
 
 interface ProductProps {
@@ -9,5 +13,65 @@ interface ProductProps {
 export const PopularCollections = ({
   products,
 }: ProductProps) => {
-  return <div>Popular Collections</div>;
+  return (
+    <section className="mt-16">
+      <div>
+        <div>
+          <h1 className="md:text-2xl font-semibold">
+            Popular Collections
+          </h1>
+        </div>
+        <div className="space-y-5 grid grid-cols-2 gap-6 items-center mt-4">
+          {products.map((product) => (
+            <div
+              key={product.id}
+              className="bg-[#F8F8FA] relative"
+            >
+              <div className="flex items-center justify-center">
+                <Image
+                  src={product.images[0]?.image_path}
+                  alt="Product Images"
+                  height={300}
+                  width={300}
+                  className="h-72 w-72 object-contain"
+                />
+              </div>
+              <div className="flex items-center justify-between p-5">
+                <h2 className="text-base font-semibold">
+                  {product.name}
+                </h2>
+                <h2 className="text-base font-semibold">
+                  {formatter.format(product.price)}
+                </h2>
+              </div>
+              <div
+                role="button"
+                className="
+                  flex 
+                  items-center 
+                  gap-1.5 
+                  bg-white 
+                  py-2 
+                  px-3 
+                  rounded-full 
+                  shadow-sm
+                  hover:opacity-50
+                  transition
+                  duration-300 
+                  absolute 
+                  top-4 
+                  right-4
+                "
+              >
+                <ShoppingBag className="h-4 w-4" />
+                <span className="text-xs font-semibold">
+                  Add to Card
+                </span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };

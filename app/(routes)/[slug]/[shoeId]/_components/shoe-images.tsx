cd2eb2f9402ba3ead36758b96583b0c3dd756c48
rosanno <rosannomacabuhay@gmail.com>
2023-12-16 13:27:01 +0800

    create shoe image gallery

--- app/(routes)/[slug]/[shoeId]/_components/shoe-images.tsx
+++ app/(routes)/[slug]/[shoeId]/_components/shoe-images.tsx
@@ -1,11 +1,42 @@
 "use client";
 
-import { Image } from "@/types";
+import Image from "next/image";
+
+import { Image as ImageType } from "@/types";
+import { cn } from "@/lib/utils";
 
 interface ShoeImages {
-  images: Image[];
+  images: ImageType[];
 }
 
 export const ShoeImages = ({ images }: ShoeImages) => {
-  return <div>Shoe Images</div>;
+  return (
+    <div>
+      <div className="bg-[#EBEBEB] p-5 sm:p-6 flex items-center justify-center">
+        <Image
+          src={images[0].image_path}
+          alt="shoe images"
+          height={500}
+          width={500}
+        />
+      </div>
+      <div className="grid grid-cols-4 items-center gap-2 mt-2">
+        {images.map((image) => (
+          <div
+            key={image.id}
+            className={cn(
+              "bg-[#EBEBEB] p-3 h-24 w-full flex items-center justify-center"
+            )}
+          >
+            <Image
+              src={image.image_path}
+              alt="shoe images"
+              height={150}
+              width={150}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };

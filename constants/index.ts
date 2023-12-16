f280028c12fbabd70f7f8da7ea18537d23ddfa42
rosanno <rosannomacabuhay@gmail.com>
2023-12-16 20:32:55 +0800

    update data

--- constants/index.ts
+++ constants/index.ts
@@ -5,3 +5,71 @@ export const featured = {
   img_path:
     "https://utfs.io/f/975ad6ad-e82a-4d59-bc6f-15f80efd1009-s4pho0.png",
 };
+
+export const Links = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "Projects",
+    href: "/projects",
+  },
+  {
+    label: "Blog",
+    href: "/blog",
+  },
+  {
+    label: "Category",
+    href: "/category",
+  },
+];
+
+export const Features = [
+  {
+    label: "About Us",
+    href: "/about-us",
+  },
+  {
+    label: "Instructions",
+    href: "/instructions",
+  },
+  {
+    label: "Catalog",
+    href: "/catalog",
+  },
+  {
+    label: "Contact Us",
+    href: "/contact-us",
+  },
+];
+
+export const Company = [
+  {
+    label: "Privacy",
+    href: "/privacy",
+  },
+  {
+    label: "Terms",
+    href: "/terms",
+  },
+  {
+    label: "Security",
+    href: "/security",
+  },
+];
+
+export const LegalTerms = [
+  {
+    label: "Privacy",
+    href: "/privacy",
+  },
+  {
+    label: "Terms",
+    href: "/terms",
+  },
+  {
+    label: "Security",
+    href: "/security",
+  },
+];

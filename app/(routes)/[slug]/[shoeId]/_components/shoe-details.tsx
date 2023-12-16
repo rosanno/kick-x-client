39ba1843acffe0d81edd4dfa75072777a7712a87
rosanno <rosannomacabuhay@gmail.com>
2023-12-16 14:46:36 +0800

    update add shoe sizes

--- app/(routes)/[slug]/[shoeId]/_components/shoe-details.tsx
+++ app/(routes)/[slug]/[shoeId]/_components/shoe-details.tsx
@@ -26,6 +26,24 @@ export const ShoeDetails = ({ data }: ShoeDetailsProps) => {
           {data.description}
         </p>
       </div>
+      <div>
+        <h4 className="text-sm font-semibold mt-5">
+          Select Size
+        </h4>
+        <div className="flex items-center flex-wrap gap-2.5 mt-3">
+          {data.sizes.map((size) => (
+            <div
+              key={size.id}
+              role="button"
+              className="bg-[#F3F3F5] h-10 w-10 rounded-xl flex items-center justify-center"
+            >
+              <div className="text-[13px] font-medium">
+                {size.size}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };

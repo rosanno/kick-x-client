ede165b592adb6b487b3f6e944b34008a6aacd03
rosanno <rosannomacabuhay@gmail.com>
2023-12-16 13:51:13 +0800

    minor changes

--- app/(routes)/[slug]/[shoeId]/_components/shoe-images.tsx
+++ app/(routes)/[slug]/[shoeId]/_components/shoe-images.tsx
@@ -4,29 +4,26 @@ import Image from "next/image";
 
 import { Image as ImageType } from "@/types";
 import { cn } from "@/lib/utils";
+import { useState } from "react";
 
 interface ShoeImages {
   images: ImageType[];
 }
 
 export const ShoeImages = ({ images }: ShoeImages) => {
+  const [selectedImage, setSelectedImage] = useState(0);
+
   return (
-    <div>
-      <div className="bg-[#EBEBEB] p-5 sm:p-6 flex items-center justify-center">
-        <Image
-          src={images[0].image_path}
-          alt="shoe images"
-          height={500}
-          width={500}
-        />
-      </div>
-      <div className="grid grid-cols-4 items-center gap-2 mt-2">
-        {images.map((image) => (
+    <div className="flex flex-col-reverse md:flex-row">
+      <div className="grid grid-cols-4 md:grid-cols-1 items-center gap-2 mt-2.5 md:mt-0 md:mr-4">
+        {images.map((image, index) => (
           <div
             key={image.id}
             className={cn(
-              "bg-[#EBEBEB] p-3 h-24 w-full flex items-center justify-center"
+              "bg-[#EBEBEB] p-3 h-24 md:h-32 md:w-32 w-full flex items-center justify-center transition duration-300",
+              selectedImage === index && "ring-1 ring-black"
             )}
+            onClick={() => setSelectedImage(index)}
           >
             <Image
               src={image.image_path}
@@ -36,6 +33,15 @@ export const ShoeImages = ({ images }: ShoeImages) => {
             />
           </div>
         ))}
+      </div>
+      <div className="bg-[#EBEBEB] p-5 sm:p-6 flex items-center justify-center">
+        <Image
+          src={images[selectedImage].image_path}
+          alt="shoe images"
+          height={500}
+          width={500}
+          className="p-10"
+        />
       </div>
     </div>
   );

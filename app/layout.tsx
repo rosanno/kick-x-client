cfb0d4e0d4f56afa00d89e987fb45298c9c50a3b
rosanno <rosannomacabuhay@gmail.com>
2023-12-16 19:13:33 +0800

    create footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
 import "./globals.css";
+
 import { Nav } from "@/components/nav";
+import { Footer } from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +25,9 @@ export default function RootLayout({
         <main className="w-full max-w-7xl mx-auto px-0 md:px-4">
           {children}
         </main>
+        <div className="w-full max-w-7xl mx-auto px-0 md:px-4">
+          <Footer />
+        </div>
       </body>
     </html>
   );
---
+++ components/footer.tsx
@@ -0,0 +1,3 @@
+export const Footer = () => {
+  return <footer className="px-4 sm:px-10">Footer</footer>;
+};

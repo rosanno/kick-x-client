eaf134a7853d7838e7689dd90696c56e5ea42257
rosanno <rosannomacabuhay@gmail.com>
2023-12-19 19:01:58 +0800

    update authOptions

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,8 @@
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import type { NextAuthOptions } from "next-auth";
+import { compare } from "bcrypt";
+import axios from "axios";
 
 export const authOptions: NextAuthOptions = {
   pages: {
@@ -14,6 +16,51 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env
         .GOOGLE_CLIENT_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "Sign in",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "example@email.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) {
+          return null;
+        }
+
+        let customer;
+
+        try {
+          customer = await axios.post(
+            `${process.env.NEXT_PUBLIC_API_URL}/customers/sign-in`,
+            credentials
+          );
+        } catch (error) {
+          console.log("AUTH ERROR", error);
+        }
+
+        console.log(customer);
+
+        if (
+          !customer ||
+          !(await compare(
+            credentials.password,
+            customer?.data.password
+          ))
+        ) {
+          return null;
+        }
+
+        return {
+          id: customer?.data.id,
+          name: customer?.data.name,
+          email: customer?.data.email,
+        };
+      },
     }),
   ],
 };

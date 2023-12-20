171c7456b70ad05bca0d32190e9144c8d85a166b
rosanno <rosannomacabuhay@gmail.com>
2023-12-20 20:27:27 +0800

    wrap root layout with session provider

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -1,20 +1,29 @@
+import { getServerSession } from "next-auth";
+
+import { authOptions } from "@/lib/auth";
+import Provider from "@/context/context-provider";
+
 import { Nav } from "@/components/nav";
 import { Footer } from "@/components/footer";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
+
   return (
-    <div>
-      <Nav />
-      <main className="w-full max-w-7xl mx-auto px-0 md:px-4">
-        {children}
-      </main>
-      <div className="w-full max-w-7xl mx-auto px-0 md:px-4">
-        <Footer />
+    <Provider session={session}>
+      <div>
+        <Nav />
+        <main className="w-full max-w-7xl mx-auto px-0 md:px-4">
+          {children}
+        </main>
+        <div className="w-full max-w-7xl mx-auto px-0 md:px-4">
+          <Footer />
+        </div>
       </div>
-    </div>
+    </Provider>
   );
 }
